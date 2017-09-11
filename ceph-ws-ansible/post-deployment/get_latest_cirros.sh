c306e9be3feed14ad32b8a7b467bb2ce92172012
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2017-09-11 13:18:36 -0400

    Add files via upload

--- ceph-ws-ansible/post-deployment/get_latest_cirros.sh
+++ ceph-ws-ansible/post-deployment/get_latest_cirros.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 latest_version="$(curl http://download.cirros-cloud.net/version/released)"
 wget http://download.cirros-cloud.net/"${latest_version}"/cirros-"${latest_version}"-x86_64-disk.img
+mkdir cirros
 mv cirros-*.img cirros/
