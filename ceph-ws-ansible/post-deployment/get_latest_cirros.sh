323c0b4ea88c453160704c6519890fde25e628c3
Stacy Véronneau <sveronneau@techjunkie4hire.com>
2017-09-12 11:04:51 -0400

    Update get_latest_cirros.sh

--- ceph-ws-ansible/post-deployment/get_latest_cirros.sh
+++ ceph-ws-ansible/post-deployment/get_latest_cirros.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf cirros
 latest_version="$(curl http://download.cirros-cloud.net/version/released)"
 wget http://download.cirros-cloud.net/"${latest_version}"/cirros-"${latest_version}"-x86_64-disk.img
 mkdir cirros
